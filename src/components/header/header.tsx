84bad5c02767af429c2612d9076643581f0525d7
jsapro <77.3.77@mail.ru>
2024-03-16 16:18:49 +0300

    feature: move header to center

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -12,38 +12,40 @@ const Header = () => {
 	const isActive = (path: string) => path === pathname;
 
 	return (
-		<header className={styles.header}>
-			<Link href='/' className={styles.logoContainer}>
-				<LogoIcon className={styles.iconLogo} />
-				<p className={styles.logoText}>Проектирование жизни и бизнеса</p>
-			</Link>
-			<nav className={styles.navigation}>
-				<Link
-					href="/"
-					className={clsx(styles.link, isActive('/') && styles.link_active)}>
-					Главная
+		<header className={styles.headerContainer}>
+			<div className={styles.header}>
+				<Link href="/" className={styles.logoContainer}>
+					<LogoIcon className={styles.iconLogo} />
+					<p className={styles.logoText}>Проектирование жизни и бизнеса</p>
 				</Link>
-				<Link
-					href="/"
-					className={clsx(styles.link, isActive('/a') && styles.link_active)}>
-					Курсы
-				</Link>
-				<Link
-					href="/"
-					className={clsx(styles.link, isActive('/b') && styles.link_active)}>
-					Контакты
-				</Link>
-				<Link
-					href="/"
-					className={clsx(
-						styles.link,
-						styles.profileLink,
-						isActive('/c') && styles.link_active
-					)}>
-					<ProfileIcon className={styles.iconProfile} />
-					<span className={styles.profileLinkText}>Личный кабинет</span>
-				</Link>
-			</nav>
+				<nav className={styles.navigation}>
+					<Link
+						href="/"
+						className={clsx(styles.link, isActive('/') && styles.link_active)}>
+						Главная
+					</Link>
+					<Link
+						href="/"
+						className={clsx(styles.link, isActive('/a') && styles.link_active)}>
+						Курсы
+					</Link>
+					<Link
+						href="/"
+						className={clsx(styles.link, isActive('/b') && styles.link_active)}>
+						Контакты
+					</Link>
+					<Link
+						href="/"
+						className={clsx(
+							styles.link,
+							styles.profileLink,
+							isActive('/c') && styles.link_active
+						)}>
+						<ProfileIcon className={styles.iconProfile} />
+						<span className={styles.profileLinkText}>Личный кабинет</span>
+					</Link>
+				</nav>
+			</div>
 		</header>
 	);
 };
