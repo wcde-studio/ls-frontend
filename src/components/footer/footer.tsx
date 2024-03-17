dbe8395d22ad44206f084fe0125ac4c4905ff40e
root <root@debian.gog3d>
2024-03-17 23:40:51 +0400

    footer size pritter

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,81 +3,85 @@ import React from 'react';
 
 import Link from 'next/link';
 
-import WCDEIcon from '@/ui/icons/wcde-icon';
-import TelegramIcon from '@/ui/icons/telegram-icon';
-import WhatsUpIcon from '@/ui/icons/whats-up-icon';
-import VKIcon from '@/ui/icons/vk-icon';
+//import WCDEIcon from '@/ui/icons/wcde-icon';
+//import TelegramIcon from '@/ui/icons/telegram-icon';
+//import WhatsUpIcon from '@/ui/icons/whats-up-icon';
+//import VKIcon from '@/ui/icons/vk-icon';
+
+import { WCDEIcon, TelegramIcon, WhatsUpIcon, VKIcon } from '@/components/ui';
 
 const Footer = () => {
-  return (
-    <footer className={styles['footer']}>
-      <div className={styles['footer-wrapper']}>
-        <div className={styles['footer-container']}>
-          <nav className={styles['site-map-container']}>
-            <ul className={styles['site-map-links-wrapper']}>
-              <li className={styles['site-map-link']}>
-                <Link  href={'/'}>
-                  Главная
-                </Link>
-              </li>
-              <li className={styles['site-map-link']}>
-                <Link href={'/'}>
-                  Курсы
-                </Link>
-              </li>
-              <li className={styles['site-map-link']}>
-                <Link href={'/'}>
-                  Контакты
-                </Link>
-              </li>
-              <li className={styles['site-map-link']}>
-                <Link href={'/'}>
-                  Личный кабинет
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <div className={styles['social-links-container']}>
-            <ul className={styles['social-links-wrapper']}>
-              <li className={styles['social-link']}>
-                <Link href={'/'}>
-                  <TelegramIcon />
-                </Link>
-              </li>
-              <li className={styles['social-link']}>
-                <Link href={'/'}>
-                  <WhatsUpIcon />
-                </Link>
-              </li>
-              <li className={styles['social-link']}>
-                <Link href={'/'}>
-                  <VKIcon />
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className={styles['attention']}>
-          <p className={styles['attention-text']}>{'Любое копирование материалов сайта допускается только с разрешения правообладателя'}</p>
-        </div>
-      </div>
-      <div className={styles['privacy-policy']}>
-        <div className={styles['privacy-policy-wrapper']}>
-          <div className={styles['privacy-policy-link']}>
-            <Link href={'/'}>
-              Пользовательское соглашение
-            </Link>
-          </div>
-          <div className={styles['creators']}>
-            <div className={styles['creators-logo']}>
-              <WCDEIcon/>
-            </div>
-            <div className={styles['creators-name']}>Сделано WCDE.studio 2023</div>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.footerWrapper}>
+				<div className={styles.footerContainer}>
+					<nav className={styles.siteMapContainer}>
+						<ul className={styles.siteMapLinksWrapper}>
+							<li className={styles.siteMapLinkWrapper}>
+								<div className={styles.siteMapLink}>
+									<Link href={'/'}>Главная</Link>
+								</div>
+							</li>
+							<li className={styles.siteMapLinkWrapper}>
+								<div className={styles.siteMapLink}>
+									<Link href={'/'}>Курсы</Link>
+								</div>
+							</li>
+							<li className={styles.siteMapLinkWrapper}>
+								<div className={styles.siteMapLink}>
+									<Link href={'/'}>Контакты</Link>
+								</div>
+							</li>
+							<li className={styles.siteMapLinkWrapper}>
+								<div className={styles.siteMapLink}>
+									<Link href={'/'}>Личный кабинет</Link>
+								</div>
+							</li>
+						</ul>
+					</nav>
+					<div className={styles.socialLinksContainer}>
+						<ul className={styles.socialLinksWrapper}>
+							<li className={styles.socialLink}>
+								<Link href={'/'}>
+									<TelegramIcon />
+								</Link>
+							</li>
+							<li className={styles.socialLink}>
+								<Link href={'/'}>
+									<WhatsUpIcon />
+								</Link>
+							</li>
+							<li className={styles.socialLink}>
+								<Link href={'/'}>
+									<VKIcon />
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className={styles.attention}>
+					<p className={styles.attentionText}>
+						{
+							'Любое копирование материалов сайта допускается только с разрешения правообладателя'
+						}
+					</p>
+				</div>
+			</div>
+			<div className={styles.privacyPolicy}>
+				<div className={styles.privacyPolicyWrapper}>
+					<div className={styles.privacyPolicyLink}>
+						<Link href={'/'}>Пользовательское соглашение</Link>
+					</div>
+					<div className={styles.creators}>
+						<div className={styles.creatorsLogo}>
+							<WCDEIcon />
+						</div>
+						<div className={styles.creatorsName}>Сделано WCDE.studio 2023</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
