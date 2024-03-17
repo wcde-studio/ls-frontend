0698f0ab74675a7ef113575ee1e8b801a98497d1
root <root@debian.gog3d>
2024-03-17 22:08:10 +0400

    pull

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,6 @@ import React from 'react'
 //import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Footer from '@/components/footer/footer';
-
-//const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,9 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header>header</header>
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   );
