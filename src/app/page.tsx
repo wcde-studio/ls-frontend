9e1cd6b32f51e8a8a476e68043b266c5c143a964
root <root@debian.gog3d>
2024-03-18 21:10:41 +0400

    merge develop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,12 @@
-import React from "react";
+import React from 'react';
+import Header from '@/components/header/header';
+import Footer from '@/components/footer/footer';
 
-import styles from './page.module.css';
-
-import { courses } from '@/lib/courses-data';
-import { TCourse } from '@/lib/definitions';
-
-import Accordion from '@/components/accordion/accordion';
-import AccordionContent from '@/components/accordion/accordion-content';
 export default function Home() {
-  return (
-    <main>
-      <div className={styles['page-wrapper']}>
-        <div className={styles['accordion-container']}>
-        {
-          courses?.map((course: TCourse, index) => {
-            return (
-              <Accordion title={course.name} key={index}>
-                <AccordionContent/>
-              </Accordion>
-            )
-          })
-        }
-        </div>
-      </div>
-    </main>
-  );
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
 }
