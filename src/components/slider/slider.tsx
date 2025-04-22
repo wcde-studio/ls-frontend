1700615b7bd98e04e44d1fb7235bd1709a6ec98c
Oleg Gluhov <gog3d@mail.ru>
2025-04-22 19:51:50 +0400

    add slider reviews

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -16,7 +16,6 @@ import { Button } from '@/components/ui';
 import { ButtonSize, ButtonType } from '@/components/ui/button/types';
 
 import SliderReview from '@/components/services/slider-review/slider-review';
-import clsx from 'clsx';
 
 interface ISliderProps {
 	reviews: {
@@ -24,83 +23,50 @@ interface ISliderProps {
 		name: string;
 		text: string;
 	}[];
-}
+} 
 
 const Slider = (props: ISliderProps) => {
 	const { reviews } = props;
 	const reviewsRef = useRef<null | HTMLUListElement>(null);
+	const sliderWrapperRef = useRef<null | HTMLDivElement>(null);
+
 
 	useEffect(() => {
 		const refWidth = reviewsRef.current!.getBoundingClientRect().width;
-		const leftLimit = 374 - refWidth;
+		//const leftLimit = 374 - refWidth;
+		const leftLimit = refWidth;
 		const rightLimit = reviewsRef.current!.getBoundingClientRect().left;
 
-		reviewsRef.current!.onmousedown = (event) => {
+		//reviewsRef.current!.onmousedown = (event) => {
+		sliderWrapperRef.current!.onmousedown = (event) => {
 			event.preventDefault();
-			reviewsRef.current!.style.position = 'absolute';
-			reviewsRef.current!.style.zIndex = '1';
-			reviewsRef.current!.ondragstart = () => {
-				return false;
-			};
-
-			const shiftX =
-				event.clientX - reviewsRef.current!.getBoundingClientRect().left;
+			const shiftX = event.clientX - reviewsRef.current!.getBoundingClientRect().left;
 
 			const moveAt = (pageX: number, pageY: number) => {
 				const refPositionX = pageX - shiftX;
-				if (rightLimit > refPositionX && refPositionX > leftLimit) {
+				//if (rightLimit > refPositionX && refPositionX > leftLimit) {
 					reviewsRef.current!.style.left = pageX - shiftX + 'px';
-				}
+				//}
 			};
 
 			const onMouseMove = (event: any) => {
 				moveAt(event.pageX, event.pageY);
 			};
+
 			document.addEventListener('mousemove', onMouseMove);
 			document.addEventListener('mouseup', () => {
-				event.preventDefault();
 				document.removeEventListener('mousemove', onMouseMove);
+				//				reviewsRef.current!.onmouseup = null;
 			});
 		};
-
-		reviewsRef.current!.ontouchstart = (event) => {
-			event.preventDefault();
-			//			reviewsRef.current!.style.position = 'absolute';
-			//			reviewsRef.current!.style.zIndex = '1';
-			reviewsRef.current!.ondragstart = () => {
-				return false;
-			};
-
-			const shiftX =
-				event.touches[0].clientX -
-				reviewsRef.current!.getBoundingClientRect().left;
-
-			const moveAt = (pageX: number, pageY: number) => {
-				const refPositionX = pageX - shiftX;
-				if (rightLimit > refPositionX && refPositionX > leftLimit) {
-					reviewsRef.current!.style.left = pageX - shiftX + 'px';
-				}
-			};
-
-			const onTouchMove = (event: any) => {
-				moveAt(event.changedTouches[0].pageX, event.changedTouches[0].pageY);
-			};
-			document.addEventListener('touchmove', onTouchMove);
-			document.addEventListener('touchup', () => {
-				event.preventDefault();
-				//			reviewsRef.current!.style.position = 'relative';
-				//			reviewsRef.current!.style.zIndex = '0';
-				document.removeEventListener('touchmove', onTouchMove);
-			});
-		};
-	}, [reviewsRef]);
+	}, [reviewsRef, sliderWrapperRef]);
 
 	return (
 		<div className={styles.slider}>
-			<div className={clsx(styles.carousel, styles.sliderWrapper)}>
-				<ul ref={reviewsRef} className={clsx(styles.track, styles.reviews)}>
+			<div ref={sliderWrapperRef} className={styles.sliderWrapper}>
+				<ul ref={reviewsRef} className={styles.reviews}>
 					{reviews.map((review) => (
-						<li className={clsx(styles.card, styles.review)} key={review.id}>
+						<li className={styles.review} key={review.id}>
 							<SliderReview
 								name={`${review.name} ${review.id}`}
 								text={review.text}
